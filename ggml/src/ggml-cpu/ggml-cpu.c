59adfdd660576501d6c0066ec998d9672554c75d
Jack <jack@trefon.com>
2026-08-05 09:51:57 +0200

    ci: remove unreachable break after noreturn GGML_ABORT (clang -Werror)

--- ggml/src/ggml-cpu/ggml-cpu.c
+++ ggml/src/ggml-cpu/ggml-cpu.c
@@ -2058,7 +2058,7 @@ static void ggml_compute_forward(struct ggml_compute_params * params, struct ggm
         case GGML_OP_GATED_DELTA_NET_PIPE:
             {
                 GGML_ABORT("GGML_OP_GATED_DELTA_NET_PIPE is CUDA-only (fused DeltaNet chunk pipeline); GDN models require GPU layers (e.g. -ngl 99)\n");
-            } break;
+            }
         case GGML_OP_MAP_CUSTOM1:
             {
                 ggml_compute_forward_map_custom1(params, tensor);
